3d09f9b58b23bdcb401b24c1f481c49eb7f51314
hissapinto <isabelahissap@gmail.com>
2026-05-12 21:08:25 -0300

    projeto ios criado

---
+++ ios/EasyFriend/EasyFriend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyFriend
+//
+//  Created by Isabela Hissa Pinto on 12/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/EasyFriend/EasyFriend/EasyFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyFriendApp.swift
+//  EasyFriend
+//
+//  Created by Isabela Hissa Pinto on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EasyFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
